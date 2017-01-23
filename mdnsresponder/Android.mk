93369aba36b898db3214eda27ece295b8485a173
wanghuan <wanghuan@pset.suntec.net>
2017-01-23 15:24:39 +0800

    porting edition 540 to Android
    Change-Id: Iac3ebca817458aff92aff60f421b0cd35331b722

--- mdnsresponder/Android.mk
+++ mdnsresponder/Android.mk
@@ -1,6 +1,6 @@
 LOCAL_PATH := $(call my-dir)
 
-#########################
+##############  mdnsd  #################
 
 include $(CLEAR_VARS)
 LOCAL_SRC_FILES :=  mDNSPosix/PosixDaemon.c    \
@@ -14,7 +14,10 @@ LOCAL_SRC_FILES :=  mDNSPosix/PosixDaemon.c    \
                     mDNSShared/mDNSDebug.c     \
                     mDNSShared/dnssd_ipc.c     \
                     mDNSShared/GenLinkedList.c \
-                    mDNSShared/PlatformCommon.c
+                    mDNSShared/PlatformCommon.c \
+                    mDNSCore/anonymous.c        \
+                    mDNSCore/CryptoAlg.c     
+
 
 LOCAL_MODULE := mdnsd
 LOCAL_MODULE_TAGS := optional
@@ -23,12 +26,26 @@ LOCAL_C_INCLUDES := external/mdnsresponder/mDNSPosix \
                     external/mdnsresponder/mDNSCore  \
                     external/mdnsresponder/mDNSShared
 
-LOCAL_CFLAGS := -O2 -g -W -Wall -D__ANDROID__ -D_GNU_SOURCE -DHAVE_IPV6 -DNOT_HAVE_SA_LEN -DUSES_NETLINK -DTARGET_OS_LINUX -fno-strict-aliasing -DHAVE_LINUX -DMDNS_DEBUGMSGS=0 -DMDNS_UDS_SERVERPATH=\"/dev/socket/mdnsd\" -DMDNS_USERNAME=\"mdnsr\" -DPLATFORM_NO_RLIMIT
+LOCAL_CFLAGS := -O2 -g -W -Wall 
+LOCAL_CFLAGS += -D__ANDROID__  
+LOCAL_CFLAGS += -D_GNU_SOURCE 
+LOCAL_CFLAGS += -DHAVE_IPV6 
+LOCAL_CFLAGS += -DNOT_HAVE_SA_LEN 
+LOCAL_CFLAGS += -DUSES_NETLINK 
+LOCAL_CFLAGS += -DTARGET_OS_LINUX 
+LOCAL_CFLAGS += -fno-strict-aliasing 
+LOCAL_CFLAGS += -DHAVE_LINUX 
+LOCAL_CFLAGS += -DMDNS_DEBUGMSGS=0 
+LOCAL_CFLAGS += -DMDNS_UDS_SERVERPATH=\"/dev/socket/mdnsd\" 
+LOCAL_CFLAGS += -DMDNS_USERNAME=\"mdnsr\" 
+LOCAL_CFLAGS += -DPLATFORM_NO_RLIMIT 
+
+
 LOCAL_SYSTEM_SHARED_LIBRARIES := libc libcutils
 
 include $(BUILD_EXECUTABLE)
 
-##########################
+#############  libmdnssd  #############
 
 include $(CLEAR_VARS)
 LOCAL_SRC_FILES :=  mDNSShared/dnssd_clientlib.c  \
@@ -38,12 +55,24 @@ LOCAL_SRC_FILES :=  mDNSShared/dnssd_clientlib.c  \
 LOCAL_MODULE := libmdnssd
 LOCAL_MODULE_TAGS := optional
 
-LOCAL_CFLAGS := -O2 -g -W -Wall -D__ANDROID__ -D_GNU_SOURCE -DHAVE_IPV6 -DNOT_HAVE_SA_LEN -DUSES_NETLINK -DTARGET_OS_LINUX -fno-strict-aliasing -DHAVE_LINUX -DMDNS_UDS_SERVERPATH=\"/dev/socket/mdnsd\" -DMDNS_DEBUGMSGS=0
+LOCAL_CFLAGS := -O2 -g -W -Wall 
+LOCAL_CFLAGS += -D__ANDROID__ 
+LOCAL_CFLAGS += -D_GNU_SOURCE 
+LOCAL_CFLAGS += -DHAVE_IPV6 
+LOCAL_CFLAGS += -DNOT_HAVE_SA_LEN 
+LOCAL_CFLAGS += -DUSES_NETLINK 
+LOCAL_CFLAGS += -DTARGET_OS_LINUX 
+LOCAL_CFLAGS += -fno-strict-aliasing 
+LOCAL_CFLAGS += -DHAVE_LINUX 
+LOCAL_CFLAGS += -DMDNS_UDS_SERVERPATH=\"/dev/socket/mdnsd\" 
+LOCAL_CFLAGS += -DMDNS_DEBUGMSGS=0
+
+
 LOCAL_SYSTEM_SHARED_LIBRARIES := libc libcutils
 
 include $(BUILD_SHARED_LIBRARY)
 
-############################
+############    dnssd     ################
 
 include $(CLEAR_VARS)
 LOCAL_SRC_FILES :=  Clients/dns-sd.c \
@@ -54,7 +83,19 @@ LOCAL_MODULE_TAGS := optional
 
 LOCAL_C_INCLUDES := external/mdnsresponder/mDNSShared
 
-LOCAL_CFLAGS := -O2 -g -W -Wall -D__ANDROID__ -D_GNU_SOURCE -DHAVE_IPV6 -DNOT_HAVE_SA_LEN -DUSES_NETLINK -DTARGET_OS_LINUX -fno-strict-aliasing -DHAVE_LINUX -DMDNS_UDS_SERVERPATH=\"/dev/socket/mdnsd\" -DMDNS_DEBUGMSGS=0
+LOCAL_CFLAGS := -O2 -g -W -Wall 
+LOCAL_CFLAGS += -D__ANDROID__ 
+LOCAL_CFLAGS += -D_GNU_SOURCE 
+LOCAL_CFLAGS += -DHAVE_IPV6 
+LOCAL_CFLAGS += -DNOT_HAVE_SA_LEN 
+LOCAL_CFLAGS += -DUSES_NETLINK 
+LOCAL_CFLAGS += -DTARGET_OS_LINUX 
+LOCAL_CFLAGS += -fno-strict-aliasing 
+LOCAL_CFLAGS += -DHAVE_LINUX 
+LOCAL_CFLAGS += -DMDNS_UDS_SERVERPATH=\"/dev/socket/mdnsd\" 
+LOCAL_CFLAGS += -DMDNS_DEBUGMSGS=0
+
+
 
 LOCAL_SYSTEM_SHARED_LIBRARIES := libmdnssd libc libcutils
 
